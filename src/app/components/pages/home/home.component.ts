e487d6bfb68d9ebb7a92d90cdbfad590fc26f815
sroohandeh <s.roohandeh74@gmail.com>
2023-07-18 17:55:59 +0330

    set search box

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FoodService } from 'src/app/services/food.service';
+
+import { ActivatedRoute } from '@angular/router';
 import { Food } from 'src/app/shared/models/Food';
+import { FoodService } from 'src/app/services/food.service';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +12,16 @@ import { Food } from 'src/app/shared/models/Food';
 export class HomeComponent implements OnInit {
   foods:Food[] = [];
 
-  constructor(private foodService:FoodService){
-    this.foods = foodService.getAll(); 
+  constructor(private foodService:FoodService, activatedRoute: ActivatedRoute){
+    activatedRoute.params.subscribe(params => {
+      if(params.searchTerm)
+        this.foods = foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else
+        this.foods = foodService.getAll(); 
+      
+
+    })
+  
   }
   ngOnInit(): void {    
   }  
--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,6 +1,6 @@
+import { Food } from '../shared/models/Food';
 import { Injectable } from '@angular/core';
 import { sample_foods } from 'src/data';
-import { Food } from '../shared/models/Food';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +11,9 @@ export class FoodService {
 
   getAll(): Food[] {
     return sample_foods
+  }
+
+  getAllFoodsBySearchTerm(searchTerm: string){
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
   }
 }
