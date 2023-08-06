9e72bab4fda68c41dec1c8f7fb2ae8c79c2236fb
sroohandeh <s.roohandeh74@gmail.com>
2023-08-06 22:10:29 +0330

    cart page UI

--- src/app/components/pages/cart-page/cart-page.component.ts
+++ src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from 'src/app/shared/models/CartItem';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.scss']
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cart!: Cart;
 
+  constructor(private cartService: CartService){
+    this.cartService.getCartObservable().subscribe((cart) =>
+    {
+      this.cart = cart;
+    } )
+  }
+
+  removeFromCart(cartItem : CartItem){
+    this.cartService.removeFromCart(cartItem.food.id)
+
+  }
+
+  changeQuantity(cartItem:CartItem, quantityString: string){
+    const quantity = parseInt(quantityString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity)
+  }
 }
